c4a54d72105da315488ff36b6ccc283b54f70659
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-02-06 22:45:20 +0800

    classes\Block.js: Added new class

---
+++ classes/Block.js
@@ -0,0 +1,58 @@
+class Block {
+	constructor( x, y ) {
+		this.x = x
+		this.y = y
+		this.angle = 0
+		this.stroke = 70
+	}
+
+	display() {
+		noFill()
+		stroke(this.stroke)
+		// Saves the current transformations (translate and rotate)
+		push()
+		// Translate the origin point
+		translate( this.x, this.y )
+		rotate( this.angle )
+		if( this.angle > 0 && this.angle < 45 )
+			this.drawRect()
+		else
+			this.drawX()
+		// Restores the transformations to the original origin point of the canvas
+		pop()
+	}
+
+	move() {
+		// Only start incrementing the angle if the mouse has moved 
+		if( ( pmouseX - mouseX != 0 ) || ( pmouseY - mouseY != 0 ) ) {
+			if( dist( mouseX, mouseY, this.x, this.y ) < distMouse ) {
+				this.angle += 1
+				// Turn stroke to white when mouse approaches
+				this.stroke = 255
+			}
+		} 
+
+		// If block has started rotating, continue until it reaches 90 degrees
+		if( this.angle > 0 && this.angle < 90 ) {
+			this.angle += 1
+			// Decrease stroke as block rotates, only if stroke is more than the original color
+			if( this.stroke > 70 )
+				this.stroke -= 3
+		}
+		else {
+			this.angle = 0
+			// Reset stroke to original color
+			this.stroke = 70
+		}
+	}
+
+	drawRect() {
+		rect( 0, 0, ( size - offset ), ( size - offset ) )
+	}
+
+	drawX() {
+		let margin = -size / 2
+		line( ( margin + offset / 2 ), ( margin + offset / 2 ), ( margin + size - offset / 2 ), ( margin + size - offset / 2 ) )
+		line( ( margin + size - offset / 2 ), ( margin + offset / 2 ), ( margin + offset / 2 ), ( margin + size - offset / 2 ) )
+	}
+}
---
+++ rotating-blocks/sketch.js
@@ -0,0 +1,36 @@
+let cols, rows
+let distMouse = 20
+let size = 10
+let offset = 4
+let blocks = []
+
+
+function setup() {
+  createCanvas( windowWidth, windowHeight )
+  // createCanvas( 400, 400 )
+  // set rectangle mode to center
+  rectMode(CENTER)
+  // set angle mode to center
+  angleMode(DEGREES)
+  cols = width / size
+  rows = height / size
+
+  for( let i = 0; i < cols; i++ ) {
+    blocks[i] = []
+    for( let j = 0; j < rows; j++ ) {
+      blocks[ i ][ j ] = new Block( ( size / 2 ) + ( i * size ), ( size / 2 ) +  ( j * size ) )
+    }
+  }
+}
+
+function draw() {
+  background(0)
+
+  for( let i = 0; i < cols; i++ ) {
+    for( let j = 0; j < rows; j++ ) {
+      block = blocks[ i ][ j ]
+      block.move()
+      block.display()
+    }
+  }
+}
